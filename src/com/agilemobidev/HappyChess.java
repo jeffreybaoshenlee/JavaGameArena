f473d2ba971b1f2e4ffee3093ff00d6338b6dd4c
jeffrey <jeffrey@jeffrey-laptop-ubuntu>
2015-04-17 17:39:53 +0800

    Modify Project's Main Java File.

--- src/com/agilemobidev/HappyChess.java
+++ src/com/agilemobidev/HappyChess.java
@@ -4,5 +4,6 @@ public class HappyChess {
     public static void main(String[] args) {
         System.out.println("Hello, Java.");
         System.out.println("This line is added via GitHub's text editor.");
+        System.out.println("This line is added in Netbeans.");
     }
 }
