8db064fb8ef6495f31dcec4761e3e43e6e68310c
jeffrey <jeffrey@jeffrey-laptop-ubuntu>
2015-04-17 17:55:30 +0800

    Modify Project's Java Files.

--- src/com/agilemobidev/HappyChess.java
+++ src/com/agilemobidev/HappyChess.java
@@ -5,5 +5,6 @@ public class HappyChess {
         System.out.println("Hello, Java.");
         System.out.println("This line is added via GitHub's text editor.");
         System.out.println("This line is added in Netbeans.");
+        System.out.println("This line is also added in Netbeans.");
     }
 }
