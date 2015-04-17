2633f804b94e4fef7a874007eee6c1895d2ba88f
Jeffrey Bao-shen LEE <eastarstormlee@gmail.com>
2015-04-17 16:48:31 +0800

    Update main java file in GitHub's editor

--- src/com/agilemobidev/HappyChess.java
+++ src/com/agilemobidev/HappyChess.java
@@ -3,5 +3,6 @@ package com.agilemobidev;
 public class HappyChess {
     public static void main(String[] args) {
         System.out.println("Hello, Java.");
+        System.out.println("This line is added via GitHub's text editor.");
     }
 }
